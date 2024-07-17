ba6fc6bd2117d22907dd9b0b9a1963d2a4246582
Kevin Christ <kevin.christ@stud.uni-heidelberg.de>
2024-07-17 10:24:34 +0200

    hooking the elements and adding more infos

--- test.js
+++ test.js
@@ -1,4 +1,9 @@
 async function fetchData() {
+  const errorMessage = document.getElementById("error");
+  const productName = document.getElementById("productName");
+  const gradeHeading = document.getElementById("gradeHeading");
+  const nutriGrade = document.getElementById("grade");
+
   try {
     const productCode = document.getElementById("barcode").value;
 
@@ -9,11 +14,20 @@ async function fetchData() {
       throw new Error("Could not find the product");
     }
     const data = await response.json();
-    const nutriScore = data.product.nutriscore_score;
-    const textScore = document.getElementById("nutriScore");
+    errorMessage.textContent = "";
+    console.log(data);
 
-    textScore.innerHTML = nutriScore;
+    // Name of the Product
+    productName.textContent = data.product.product_name || "Name not available";
+
+    // Nutriscore grade of the Product
+    gradeHeading.textContent = "Nutriscore Grade:";
+    nutriGrade.textContent = data.product.nutriscore_grade || "Not available";
   } catch (error) {
     console.error(error);
+    productName.textContent = "";
+    gradeHeading.textContent = "";
+    nutriGrade.textContent = "";
+    errorMessage.textContent = "Could not find the product";
   }
 }
